21a223d26608070f3b38d6c81e34b2605a1c514e
Nicky Mirz <nickymirz@gmail.com>
2020-07-11 12:32:10 +0300

    Updates

---
+++ precache-manifest.6c94735e56cbdff9a6543c1a89896dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4515087473b36577ef363a658141ee63",
+    "url": "/wargame/index.html"
+  },
+  {
+    "revision": "1723d736c5c1ed62c868",
+    "url": "/wargame/static/css/main.df9decc2.chunk.css"
+  },
+  {
+    "revision": "db96f8da13e7c1a1daa1",
+    "url": "/wargame/static/js/2.cbe91756.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wargame/static/js/2.cbe91756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1723d736c5c1ed62c868",
+    "url": "/wargame/static/js/main.d6f3d32d.chunk.js"
+  },
+  {
+    "revision": "2f558b1d7690a8dd92fa",
+    "url": "/wargame/static/js/runtime-main.d5684212.js"
+  }
+]);
